ad1217182197dc0f2a68976458571728244b8ef2
libertinistic <libertinistic@gmail.com>
2016-08-07 18:36:37 -0700

    added initial implementation of user model tests

--- test/models.test.js
+++ test/models.test.js
@@ -3,8 +3,64 @@ const assert = require('chai').assert;
 const Secret = require('../lib/models/secretModel');
 const User = require('../lib/models/userModel');
 
-describe('secret model', () => {
-  it.skip ('', () => {
-    assert.ok();
+describe('user', () => {
+  it('requires text', (done) => {
+    const user = new User();
+    user.validate((err) => {
+      if (!err) done('secretModel should require text.');
+      else done();
+    });
+  });
+
+  const rupSecret = new Secret({
+
+  });
+  const rupert = new User({
+    name: {
+      first: 'Rupert',
+      last: 'Thrumboldt'
+    },
+    contact: {
+      email: 'thrumblers@gmail.com',
+      phone: '867-5309'
+    },
+    criteria: {
+      gender: 'male',
+      orientation: 'gay',
+      interestedIn: 'men'
+    },
+    secret: '' //TODO: Populate this.
+  });
+
+  // user : {
+  //   "name" : {
+  //     "first" : "kate",
+  //     "last" : "sowles"
+  //   },
+  //   "contact" : {
+  //     "email" : "",
+  //     "phone" : "",
+  //     "location" : ""
+  //   },
+  //   "criteria" : {
+  //     "gender" : "Female",
+  //     "orientation" : "Straight",
+  //     "interestedIn" : "Men"
+  //   },
+  //   "secret" : sekrit123._id
+  //   }
+  // }
+});
+
+describe('secret', () => {
+  const badSecret = new Secret();
+
+  it('requires fields to process', (done) => {
+    badSecret.validate((err) => {
+      if (!err) done('secretModel should reject without required fields.');
+      else done();
+    });
+
+
   });
 });
