4f0e08129c4008a4cd3de23d9bdb676d64ef6fa1
libertinistic <libertinistic@gmail.com>
2016-08-07 17:00:45 -0700

    amended eslintrc rules to account for windows dev env. added model test file.

---
+++ test/models.test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+
+const Secret = require('../lib/models/secretModel');
+const User = require('../lib/models/userModel');
+
+describe('secret model', () => {
+  it.skip ('', () => {
+    assert.ok();
+  });
+});
