1866f483bff1c81db01f322c9ca420d3a844ac87
libertinistic <libertinistic@gmail.com>
2016-08-08 11:29:32 -0700

    added watch to package.json

--- test/models.test.js
+++ test/models.test.js
@@ -1,9 +1,17 @@
-const assert = require('chai').assert;
+const chai = require('chai');
+const mongoose = require('mongoose');
+const chaiHTTP = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHTTP);
+
+const connection = require('../lib/setup-mongoose');
 
 const Secret = require('../lib/models/secretModel');
 const User = require('../lib/models/userModel');
 
 describe('user', () => {
+
+
   it('requires text', (done) => {
     const user = new User();
     user.validate((err) => {
@@ -12,44 +20,38 @@ describe('user', () => {
     });
   });
 
-  const rupSecret = new Secret({
+  // const rupSecret = new Secret({
+  //   text: 'Sometimes I set the drapes aflame to teach the help a lesson.'
+  // });
 
-  });
-  const rupert = new User({
-    name: {
-      first: 'Rupert',
-      last: 'Thrumboldt'
-    },
-    contact: {
-      email: 'thrumblers@gmail.com',
-      phone: '867-5309'
-    },
-    criteria: {
-      gender: 'male',
-      orientation: 'gay',
-      interestedIn: 'men'
-    },
-    secret: '' //TODO: Populate this.
+  // const rupert = new User({
+  //   name: {
+  //     first: 'Rupert',
+  //     last: 'Thrumboldt'
+  //   },
+  //   contact: {
+  //     email: 'thrumblers@gmail.com',
+  //     phone: '867-5309'
+  //   },
+  //   criteria: {
+  //     gender: 'male',
+  //     orientation: 'gay',
+  //     interestedIn: 'men'
+  //   },
+  //   secret: '' //TODO: Populate this.
+  // });
+
+  it.skip('requires text', (done) => {
+    const user = new User();
+    user.validate((err) => {
+      if (!err) done('secretModel should require text.');
+      else done();
+    });
   });
 
-  // user : {
-  //   "name" : {
-  //     "first" : "kate",
-  //     "last" : "sowles"
-  //   },
-  //   "contact" : {
-  //     "email" : "",
-  //     "phone" : "",
-  //     "location" : ""
-  //   },
-  //   "criteria" : {
-  //     "gender" : "Female",
-  //     "orientation" : "Straight",
-  //     "interestedIn" : "Men"
-  //   },
-  //   "secret" : sekrit123._id
-  //   }
-  // }
+  after('remove mongoose model', () => {
+    delete mongoose.connection.models['User'];
+  });
 });
 
 describe('secret', () => {
@@ -61,6 +63,10 @@ describe('secret', () => {
       else done();
     });
 
+    after('remove mongoose model', () => {
+      delete mongoose.connection.models['Secret'];
 
+      mongoose.connection.close();
+    });
   });
 });
