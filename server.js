1c223b2408c3fe8bb877ee27d614ebb92b336e0a
libertinistic <libertinistic@gmail.com>
2016-08-09 22:18:49 -0700

    added a basic stab at automotically generating fake data on app launch. Never be without secrets again!

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const app = require('./lib/app');
 const http = require('http');
 const port = process.env.PORT || 3000;
+//Next two lines should be removed when we're done using false data.
+const fakeData = require('./lib/fakeData');
+const User = require('./lib/models/userModel');
 require('./lib/setup-mongoose');
 
 const server = http.createServer(app);
 
 server.listen(port, ()=>{
   console.log('server started on ', server.address().port);
+});
+
+//TODO: I'm reasonably sure that the password hashing pattern here is wrong.
+fakeData(35).forEach((user) => {
+  let fakeUser = new User(user);
+  fakeUser.generateHash(fakeUser.password);
+  fakeUser.save();
 });
